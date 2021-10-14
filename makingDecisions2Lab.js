66408afb1edcf7715816d1a50dc48c0c08e8ea5f
Timothy Luebke <luebketj@gmail.com>
2021-10-14 14:22:30 -0600

    finished problem 4 also made it print it out, I know you didnt ask for that. but Im a little extra

--- makingDecisions2Lab.js
+++ makingDecisions2Lab.js
@@ -45,7 +45,12 @@ let evensArr = []
 */
 
 //Code Here
-
+for(let i = 0; i < nums.length; i ++){
+  if(nums[i] % 2 === 0){
+    evensArr.push(nums[i]);
+  }
+}
+console.log(evensArr);
 
 
 ////////// PROBLEM 5 //////////
