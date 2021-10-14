fb731d43519ff18e9800ff19c4aabe9a4d43f355
Timothy Luebke <luebketj@gmail.com>
2021-10-14 14:19:50 -0600

    finished problem 3 : )

--- makingDecisions2Lab.js
+++ makingDecisions2Lab.js
@@ -28,7 +28,9 @@ var family = ['Tyler', 'Jordan', 'Ryan', 'Alice', 'Ireland'];
 */
 
 //Code Here
-
+for(let i = 0; i < family.length; i ++){
+  console.log(family[i]);
+}
 
 
 ////////// PROBLEM 4 //////////
